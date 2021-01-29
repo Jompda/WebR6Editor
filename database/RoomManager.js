c29ab49c0b9b75fd263de4eaafd01d9eb9f64d4f
Jompda <jonirapo@gmail.com>
2021-01-29 23:45:30 +0200

    Removed unneeded semicolons.

--- database/RoomManager.js
+++ database/RoomManager.js
@@ -37,13 +37,13 @@ function saveSlide(room, slideName, saveData, request, response) {
  * @returns {Room|false}
  */
 function roomAccess(roomName, request) {
-	const room = rooms.find((room) => room.name === roomName);
-	if (!room || !request.headers.authorization) return false;
+	const room = rooms.find((room) => room.name === roomName)
+	if (!room || !request.headers.authorization) return false
 
-	const basicAuth = request.headers.authorization.slice('Basic '.length);
-	const password = Buffer.from(basicAuth, 'base64').toString();
+	const basicAuth = request.headers.authorization.slice('Basic '.length)
+	const password = Buffer.from(basicAuth, 'base64').toString()
 
-	return password === room.password ? room : false;
+	return password === room.password ? room : false
 }
 
 module.exports = {
